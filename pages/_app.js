0b077327b63d1f4c68e38f8d1847c2d38d108866
ref3at-13 <ref3at.13@gmail.com>
2025-08-03 18:41:01 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '../styles/globals.css';
+// pages/_app.js
+import Head from 'next/head'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* change href to match your filename */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
